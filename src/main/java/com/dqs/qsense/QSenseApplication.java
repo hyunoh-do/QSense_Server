5b11e3ce9b9afc064d6ef6039b5bda243bf77c30
hyunoh-do <hyunoh.do@gmail.com>
2023-02-27 09:55:03 +0900

    init - Create Project

---
+++ src/main/java/com/dqs/qsense/QSenseApplication.java
@@ -0,0 +1,13 @@
+package com.dqs.qsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QSenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QSenseApplication.class, args);
+    }
+
+}
